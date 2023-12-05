2230d3cd1cb30b3cb819e61a4c15e1c43237ada8
Marek Kaluba <kalmar@amu.edu.pl>
2023-12-05 14:00:28 +0100

    reorganize tests a bit

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,23 +9,17 @@ include(joinpath(pkgdir(AbstractPermutations), "test", "perms_by_images.jl"))
 import .ExamplePerms as EP
 
 @testset "AbstractPermutations.jl" begin
-    @testset "incomplete implementation" begin
-        struct APerm <: AP.AbstractPermutation end
-
-        p = APerm()
-        @test_throws AP.InterfaceNotImplemented AP.degree(p)
-        @test_throws AP.InterfaceNotImplemented 3^p
-    end
-
     include("example_perms_tests.jl")
 
+    @testset "incomplete implementation" begin
+        include("aperm_interface_check.jl")
+        import .APerms.APerm as APerm
+
+        abstract_perm_interface_test(APerm)
+
+        @test convert(APerm, EP.Perm([2, 3, 1])) isa APerm
+        @test convert(APerm, EP.Perm([2, 3, 1])) == EP.Perm([2, 3, 1])
+    end
+
     include("parsing.jl")
-
-    include("aperm_interface_check.jl")
-
-    import .APerms
-    abstract_perm_interface_test(APerms.APerm)
-
-    @test convert(APerm, EP.Perm([2, 3, 1])) isa APerm
-    @test convert(APerm, EP.Perm([2, 3, 1])) == EP.Perm([2, 3, 1])
 end
