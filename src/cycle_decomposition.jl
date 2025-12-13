fbbe71fdbc9d108ff3b461329ec1833fd3bc0c9e
matthias314 <56549971+matthias314@users.noreply.github.com>
2025-12-13 07:27:39 -0500

    made `CycleDecomposition` a subtype of `AbstractVector` (#25)

--- src/cycle_decomposition.jl
+++ src/cycle_decomposition.jl
@@ -1,20 +1,20 @@
-struct CycleDecomposition{T<:Integer}
+struct CycleDecomposition{T<:Integer} <:
+       AbstractVector{SubArray{T,1,Vector{T},Tuple{UnitRange{Int64}},true}}
     cycles::Vector{T} # cycles, concatenated
     cycles_ptrs::Vector{T} # pointers to the starts of the cycles
 end
 
 degree(cd::CycleDecomposition) = length(cd.cycles)
 
-Base.length(cd::CycleDecomposition) = length(cd.cycles_ptrs) - 1
-function Base.eltype(::Type{CycleDecomposition{T}}) where {T}
-    return SubArray{T,1,Vector{T},Tuple{UnitRange{Int64}},true}
-end
+Base.size(cd::CycleDecomposition) = (length(cd.cycles_ptrs) - 1,)
 
-function Base.iterate(cd::CycleDecomposition, state = 1)
-    state == length(cd.cycles_ptrs) && return nothing
-    from = cd.cycles_ptrs[state]
-    to = cd.cycles_ptrs[state+1] - 1
-    return @inbounds @view(cd.cycles[from:to]), state + 1
+Base.IndexStyle(::Type{<:CycleDecomposition}) = IndexLinear()
+
+@inline function Base.getindex(cd::CycleDecomposition, i::Int)
+    @boundscheck checkbounds(cd, i)
+    @inbounds from = cd.cycles_ptrs[i]
+    @inbounds to = cd.cycles_ptrs[i+1] - 1
+    return @inbounds @view(cd.cycles[from:to])
 end
 
 function Base.show(io::IO, cd::CycleDecomposition)
